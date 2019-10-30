f9be2c304c3298b4e777f05c390ba3ffd76dc3dc
khalid7487 <khalid15-7487@diu.edu.bd>
2019-10-30 14:04:21 +0600

    bean model

--- Composite_componentwithJSF/src/java/bean/Person.java
+++ Composite_componentwithJSF/src/java/bean/Person.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bean;
 
-/**
- *
- * @author KHALID
- */
-public class Person {
-    
+import javax.annotation.ManagedBean;
+import javax.faces.bean.ApplicationScoped;
+
+
+@ManagedBean
+@ApplicationScoped
+public class Person extends Programmer{
+    public Person(){
+        super("Md.","Khalid","Jounior","HTML","CSS","JavaScript","Oracle","Java","XML","Android");
+    }
 }
--- Composite_componentwithJSF/src/java/bean/Programmer.java
+++ Composite_componentwithJSF/src/java/bean/Programmer.java
@@ -5,10 +5,49 @@
  */
 package bean;
 
-/**
- *
- * @author KHALID
- */
+
+
 public class Programmer {
+    private String firstName,lastName,level;
+    private String[] languages;
+
+    public Programmer(String firstName, String lastName, String level, String... languages) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.level = level;
+        this.languages = languages;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
     
 }
